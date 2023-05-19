d285bf1a19f957fe4a1dedb5fb6b83f6f1f20d64
Pluttooo96 <pustulm96@gmail.com>
2023-05-19 03:43:33 +0200

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 import App from './App';
 
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
 );
